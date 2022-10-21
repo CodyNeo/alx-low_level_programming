3d2fbb14c4db40cf1442bcb4c775e58800408b0d
CodyLeo <abdallahomm123@gmail.com>
2022-10-21 02:45:46 -0700

    long

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-include "stdio.h"
+#include <stdio.h>
+#include <string.h>
+
 int main(void){
-	puts("Programming is like building a multilingual puzzle");
-}
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+	}
